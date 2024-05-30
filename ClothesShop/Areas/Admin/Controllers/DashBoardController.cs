830389bb618fe98cce2e4899ef4802206d66240c
Gunel25 <cabbarligunel0899@gmail.com>
2024-05-30 10:05:27 +0400

    AdminLogout

--- ClothesShop/Areas/Admin/Controllers/DashBoardController.cs
+++ ClothesShop/Areas/Admin/Controllers/DashBoardController.cs
@@ -1,17 +1,33 @@
-﻿using ClothesShop.Models;
+﻿using ClothesShop.DAL;
+using ClothesShop.Extensions;
+using ClothesShop.Models;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ClothesShop.Areas.Admin.Controllers
 {
+
     [Area("Admin")]
     [Authorize(Roles = "Admin")]
     public class DashBoardController : Controller
     {
+        private readonly SignInManager<ProgramUser> _signInManager;
+        public DashBoardController(AppDbContext _appDbContext,  SignInManager<ProgramUser> signInManager)
+        {
+        
+            _signInManager = signInManager;
+          
+        }
         public IActionResult Index()
         {
             List<Slider> list = new List<Slider>();
             return View();
+        }
+      public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            return RedirectToAction("Index", "Home", new { area = "" }); // alanı belirtmiyoruz
         }
     }
 }
